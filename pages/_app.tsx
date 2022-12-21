28786d5ea9698de5ca3ea6b8b25a86d38b832c21
Sotatek-HieuLuu <hieu.luu@sotatek.com>
2022-12-21 14:39:33 +0700

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { WalletUIProvider } from '@algoscan/use-wallet-ui';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WalletUIProvider providers={['pera', 'myalgo', 'defly']}>
+      <Component {...pageProps} />
+    </WalletUIProvider>
+  );
 }
